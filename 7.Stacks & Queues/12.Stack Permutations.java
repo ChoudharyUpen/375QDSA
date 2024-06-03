a7bdb5ea08b345af7f37b2a45e87e08280898f43
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-06-03 08:40:57 +0530

    Create 12.Stack Permutations.java

---
+++ 7.Stacks & Queues/12.Stack Permutations.java
@@ -0,0 +1,84 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+        
+        return a;
+    }
+    
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+    
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int N; 
+            N = Integer.parseInt(br.readLine());
+            
+            
+            int[] A = IntArray.input(br, N);
+            
+            
+            int[] B = IntArray.input(br, N);
+            
+            Solution obj = new Solution();
+            int res = obj.isStackPermutation(N, A, B);
+            
+            System.out.println(res);
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static int isStackPermutation(int n, int[] ip, int[] op) {
+          Stack<Integer> stack = new Stack<>();
+        int j = 0; // pointer for output array
+
+        // Traverse the input array
+        for (int i = 0; i < n; i++) {
+            // Push the current element to the stack
+            stack.push(ip[i]);
+
+            // Check the top of the stack against the current element in the output array
+            while (!stack.isEmpty() && stack.peek() == op[j]) {
+                stack.pop();
+                j++;
+            }
+        }
+
+        // If the stack is empty, then the output array is a valid stack permutation of the input array
+        return stack.isEmpty() ? 1 : 0;
+        
+        
+    }
+}
+            
