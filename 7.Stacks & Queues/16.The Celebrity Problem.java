f5902d2915db70cb894a3816a547e4ce888c8118
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-06-04 13:48:02 +0530

    Create 16.The Celebrity Problem.java

---
+++ 7.Stacks & Queues/16.The Celebrity Problem.java
@@ -0,0 +1,21 @@
+class Solution
+{ 
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(int M[][], int n)
+    {
+    
+        int candidate = 0;
+        for (int i = 1; i < n; i++) {
+            if (M[candidate][i] == 1)
+                candidate = i;
+        }
+        for (int i = 0; i < n; i++) {
+            //i != candidate  kud ka saath compare nahi kar rahe
+            //check if the candiate know person i
+            //check if the person i doesnot know the candiate
+            if (i != candidate && (M[candidate][i] == 1 || M[i][candidate] == 0))
+                return -1; 
+        }
+        return candidate; 
+    }
+}
