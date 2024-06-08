e99bbe7f309d9f2264b88d82c0d443de35d6643f
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-06-08 20:17:21 +0530

    Update 11.Max length chain.java

--- 8.Greddy/11.Max length chain.java
+++ 8.Greddy/11.Max length chain.java
@@ -2,11 +2,14 @@ class GfG
 {
     int maxChainLength(Pair arr[], int n)
     {
-       // this question is nothing but Activity Selection
-       
-     // Step 1: Sorting the Array Based on 2nd Number
+        // this question is nothing but Activity Selection
+        //learned from apna college dsa course 
+        //learn more about .y what is this and overall program
+        
+       // Step 1: Sorting the Array Based on 2nd Number
         Arrays.sort(arr, Comparator.comparingInt(a -> a.y));
 
+        //Step 2 Calculating the total length of chain initlailiy it is 1.
         int chainLen = 1;
         int chainEnd = arr[0].y; // Last Selected pair end //chain end
 
