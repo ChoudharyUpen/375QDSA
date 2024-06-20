d31e8026ba15bb40f6b5312a3078d0e0ff68816e
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-06-20 08:39:06 +0530

    Update 4.Invert Binary Tree.java

--- 9.Binary Tree/4.Invert Binary Tree.java
+++ 9.Binary Tree/4.Invert Binary Tree.java
@@ -1,18 +1,9 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        //Step 1 Base Case
         if(root==null) return root;
-
-        //Step 2 Storing Left all by Recursion
         TreeNode ln=invertTree(root.left);
-        //Step 2 Storing Right all by Recursion
         TreeNode rn=invertTree(root.right);
-   
-        //Step 3 Exchanging the value of left and right
         root.left=rn;
         root.right=ln;
-
-        //Step 4 Returning the final answer
         return root;
-    }
-}
+    }}
