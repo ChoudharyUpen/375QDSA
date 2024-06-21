d87f231328fe2511e2dcfeb2192b9ab797d0f4a7
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-06-21 08:15:12 +0530

    Update 6.Left View of Binary Tree.java

--- 9.Binary Tree/6.Left View of Binary Tree.java
+++ 9.Binary Tree/6.Left View of Binary Tree.java
@@ -2,11 +2,8 @@ class Tree
 {
     ArrayList<Integer> leftView(Node root)
     {
-        //Step1 Creating a Array List which we will return as answer
        ArrayList<Integer> numbers = new ArrayList<>();
-        //Step2 this is the M.Imp thing Calling the Function
         leftViewUtil(root,numbers,0);
-        //Returning the final answer
         return numbers;
     }
     void leftViewUtil(Node root,ArrayList list,int level){
@@ -15,9 +12,7 @@ class Tree
          if(level == list.size()){
             list.add(root.data);
         }
-        //Recursion for Left
         leftViewUtil(root.left,list,level+1);
-        //Recursion for Right
         leftViewUtil(root.right,list,level+1);
     }
 }
