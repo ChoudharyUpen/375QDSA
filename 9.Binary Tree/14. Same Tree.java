3d0afa39b51333165c216caaab483d9fd0d7fda8
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-06-21 09:24:05 +0530

    Update 14. Same Tree.java

--- 9.Binary Tree/14. Same Tree.java
+++ 9.Binary Tree/14. Same Tree.java
@@ -1,17 +1,8 @@
 class Solution {
     public boolean isSameTree(TreeNode root1, TreeNode root2) {
-        //Base Case 1
-         if(root1==null && root2==null)//both trees are empty
-         return true;
-         
-         //Base Case 2
-         if(root1==null || root2==null)//one of the tree empty
-         return false;
-         
-         if(root1.val!=root2.val)
-         return false;
-         
+         if(root1==null && root2==null) return true;
+         if(root1==null || root2==null) return false;
+         if(root1.val!=root2.val) return false;
          else
          return(isSameTree(root1.right,root2.right)&& isSameTree(root1.left,root2.left));
-    }
-}
+    }}
