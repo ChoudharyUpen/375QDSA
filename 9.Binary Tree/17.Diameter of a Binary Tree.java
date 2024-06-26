c43a9bbec8a51e1f205303585cb41fa1690ef74d
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-06-26 20:40:57 +0530

    Update 17.Diameter of a Binary Tree.java

--- 9.Binary Tree/17.Diameter of a Binary Tree.java
+++ 9.Binary Tree/17.Diameter of a Binary Tree.java
@@ -1,4 +1,6 @@
 class Solution {
+    // hum isma do cheez nikal rahe hai height or diameter fir diameter 
+    //diameter as final answer return kar raha hai
     int ans = 0; 
     int diameter(Node root) {
        height(root);
@@ -8,6 +10,8 @@ class Solution {
         if (node == null) return 0;
         int lh = height(node.left);  
         int rh = height(node.right);  
+         // ye huma diameter return kar raha hai
         ans = Math.max(ans, 1 + lh + rh);  // Update the maximum diameter
+         // ye huma height return kar raha hai
         return 1 + Math.max(lh, rh);  // Return the height of the current node
     }}
