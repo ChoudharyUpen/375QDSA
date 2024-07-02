b07a4b838eb4b31558a939704299059362a0b12a
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-02 09:03:57 +0530

    Create 12.Preorder to BST.java

---
+++ 10.Binary Search Trees/12.Preorder to BST.java
@@ -0,0 +1,20 @@
+static class Solution {
+    public Node Bst(int pre[], int size) {
+        Node root=new Node(pre[0]);
+        for(int i=1;i<size;i++){
+            root=dfs(pre[i],root);
+        }return root;
+    }
+    static Node dfs(int key,Node head){
+        if(head==null){
+            Node node=new Node(key);
+            return node;
+        }
+        if(key<head.data){
+            head.left=dfs(key,head.left);
+        }else{
+            head.right=dfs(key,head.right);
+        }
+        return head;
+    }
+}
