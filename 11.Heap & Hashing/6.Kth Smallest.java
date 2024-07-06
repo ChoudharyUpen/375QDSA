1fbc3cd569b6925a7f2c7ff18f9eb9e1ed72d4b2
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-06 09:17:30 +0530

    Create 6.Kth Smallest.java

---
+++ 11.Heap & Hashing/6.Kth Smallest.java
@@ -0,0 +1,6 @@
+//dont revise dont learn this question this question is just a waste of time
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) { 
+        Arrays.sort(arr);
+        return arr[k-1];
+    } }
