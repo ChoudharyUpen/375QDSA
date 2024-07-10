361cacdd88204c231e6ce1f6f748e1317259aa32
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-10 12:15:31 +0530

    Update 4.Queue Reversal.java

--- 7.Stacks & Queues/4.Queue Reversal.java
+++ 7.Stacks & Queues/4.Queue Reversal.java
@@ -1,20 +1,11 @@
 class GfG{
     public Queue<Integer> rev(Queue<Integer> q){
-       //ek STack bana lo jis ma value dalo ga 
        Stack<Integer> stack = new Stack<>();
-         // Dequeue elements from the queue and push them onto the stack
         while (!q.isEmpty()) {
-            stack.push(q.poll());
-            
+            stack.push(q.remove());
         }
-          // Create a new queue to hold the reversed elements
-        Queue<Integer> reversedQueue = new LinkedList<>();
-        // Print the elements of the stack
-       
         while (!stack.isEmpty()) {
-           reversedQueue.offer(stack.pop());
+           q.add(stack.pop());
         }
-         // Return the reversed queue
-        return reversedQueue;
-    }
-}
+        return q;
+    }}
