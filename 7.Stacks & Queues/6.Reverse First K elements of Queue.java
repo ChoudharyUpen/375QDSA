399995b404772859ef67045cac0f4fc03caa2458
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-10 12:29:55 +0530

    Update 6.Reverse First K elements of Queue.java

--- 7.Stacks & Queues/6.Reverse First K elements of Queue.java
+++ 7.Stacks & Queues/6.Reverse First K elements of Queue.java
@@ -1,26 +1,15 @@
 class GfG {
-    
     public Queue<Integer> modifyQueue(Queue<Integer> q, int k) {
-        //ye stack jis ma value dalenge 
         Stack<Integer> stack=new Stack<>();
-        
+        Queue<Integer> newq=new LinkedList<>();
         for(int i=0;i<k;i++){
-            stack.push(q.poll());
-            
+           stack.push(q.remove());
         }
-         // Create a new queue to hold the reversed elements
-         Queue<Integer> reversedQueue=new LinkedList<>();
-         
-         while(!stack.isEmpty()){
-              reversedQueue.offer(stack.pop());
-         }
-         
-         
-         while(!q.isEmpty()){
-             reversedQueue.offer(q.poll());
-         }
-         
-          // Return the reversed queue
-        return reversedQueue;
-    }
-}
+        while (!stack.isEmpty()) {
+            newq.add(stack.pop());
+        }
+        while (!q.isEmpty()) {
+              newq.add(q.remove());
+        }
+        return newq;
+    }}
