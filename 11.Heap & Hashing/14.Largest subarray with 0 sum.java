07d22b3314fc4c861d44e6cda2b6d0437f251bc7
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-11 06:15:11 +0530

    Update 14.Largest subarray with 0 sum.java

--- 11.Heap & Hashing/14.Largest subarray with 0 sum.java
+++ 11.Heap & Hashing/14.Largest subarray with 0 sum.java
@@ -2,8 +2,7 @@ class GfG{
     //nishant chahr used same approach as me but i was not sure whether this is the ciorrect approach or not
     int maxLen(int arr[], int n){
         HashMap<Integer, Integer> mp = new HashMap<>();
-        int sum = 0;
-        int ans = 0;
+        int sum = 0, ans = 0;
         mp.put(0, -1);
         for (int i = 0; i < n; i++) {
             sum += arr[i];
@@ -11,8 +10,6 @@ class GfG{
                 ans = Math.max(ans, i - mp.get(sum));
             } else {
                 mp.put(sum, i);
-            }
-        }
+            }}
         return ans;
-    }
-}
+    }}
