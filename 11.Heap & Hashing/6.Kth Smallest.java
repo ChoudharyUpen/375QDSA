72bef5b800eae9596b3f2d81d5c2933dbb0d526b
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-11 11:00:14 +0530

    Update 6.Kth Smallest.java

--- 11.Heap & Hashing/6.Kth Smallest.java
+++ 11.Heap & Hashing/6.Kth Smallest.java
@@ -1,3 +1,11 @@
+Input:
+n = 6    arr[] = 7 10 4 3 20 15    k = 3  l=0  r=5
+Output : 
+7
+Explanation :
+3rd smallest element in the given array is 7.
+
+    
 //dont revise dont learn this question this question is just a waste of time
 class Solution{
     public static int kthSmallest(int[] arr, int l, int r, int k) { 
