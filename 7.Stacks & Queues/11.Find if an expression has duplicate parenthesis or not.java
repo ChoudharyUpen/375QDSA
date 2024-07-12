a549b3f4706cd969f5df426ada499feb48ef8c80
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-12 09:07:20 +0530

    Update 11.Find if an expression has duplicate parenthesis or not.java

--- 7.Stacks & Queues/11.Find if an expression has duplicate parenthesis or not.java
+++ 7.Stacks & Queues/11.Find if an expression has duplicate parenthesis or not.java
@@ -1,5 +1,64 @@
 import java.util.Stack;
 
+// Java program to find duplicate parenthesis in a balanced expression
+public class GFG {
+
+    // Function duplicate parenthesis find karne ke liye
+    static boolean findDuplicateparenthesis(String s) {
+        // Character ka stack banao
+        Stack<Character> Stack = new Stack<>();
+
+        // Given expression ko iterate karo
+        char[] str = s.toCharArray();
+        for (char ch : str) {
+            // Agar current character close parenthesis ')' hai
+            if (ch == ')') {
+                // Stack se character pop karo
+                char top = Stack.peek();
+                Stack.pop();
+
+                // Ye count karta hai ki closing aur opening parenthesis ke beech kitne characters hain
+                // Agar ye count 1 se kam ya barabar hai to brackets redundant hain
+                int elementsInside = 0;
+                while (top != '(') {
+                    elementsInside++;
+                    top = Stack.peek();
+                    Stack.pop();
+                }
+                if (elementsInside < 1) {
+                    return true;
+                }
+            } else { // Open parenthesis '(', operators aur operands ko stack mein push karo
+                Stack.push(ch);
+            }
+        }
+
+        // Koi duplicates nahi mile
+        return false;
+    }
+
+    // Driver code
+    public static void main(String[] args) {
+        // Input balanced expression
+        String str = "(((a+(b))+(c+d)))";
+
+        if (findDuplicateparenthesis(str)) {
+            System.out.println("Duplicate Found ");
+        } else {
+            System.out.println("No Duplicates Found ");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+import java.util.Stack;
+
 // Java program to find duplicate parenthesis in a 
 // balanced expression 
 public class GFG {
