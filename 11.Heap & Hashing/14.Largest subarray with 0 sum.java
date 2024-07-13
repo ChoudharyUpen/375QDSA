c5560a0f9f5e493c29e31f7008de86ca5597ccd6
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-13 17:34:04 +0530

    Update 14.Largest subarray with 0 sum.java

--- 11.Heap & Hashing/14.Largest subarray with 0 sum.java
+++ 11.Heap & Hashing/14.Largest subarray with 0 sum.java
@@ -1,3 +1,14 @@
+//gfg
+Input: arr[] = {15,-2,2,-8,1,7,10,23}, n = 8
+Output: 5
+Explanation: The largest subarray with sum 0 is -2 2 -8 1 7.
+    
+Input: arr[] = {2,10,4}, n = 3
+Output: 0
+Explanation: There is no subarray with 0 sum.
+
+
+
 class GfG{
     //nishant chahr used same approach as me but i was not sure whether this is the ciorrect approach or not
     int maxLen(int arr[], int n){
