306bef8d2c6620d76355eb387527e7ad103a2f02
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-23 12:12:36 +0530

    Update 1.Lowest Common Ancestor of a Binary Search Tree.java

--- 10.Binary Search Trees/1.Lowest Common Ancestor of a Binary Search Tree.java
+++ 10.Binary Search Trees/1.Lowest Common Ancestor of a Binary Search Tree.java
@@ -1,24 +1,9 @@
-//gfg
+//leetcode
 Example 1:
 
-Input:
-              5
-            /   \
-          4      6
-         /        \
-        3          7
-                    \
-                     8
-n1 = 7, n2 = 8
-Output: 7
-Example 2:
-
-Input:
-     2
-   /   \
-  1     3
-n1 = 1, n2 = 3
-Output: 2
+Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+Output: 6
+Explanation: The LCA of nodes 2 and 8 is 6.
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
