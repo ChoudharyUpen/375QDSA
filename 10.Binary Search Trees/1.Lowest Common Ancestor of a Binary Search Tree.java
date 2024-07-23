85b31e5d4aadb92a32f5f1e4fba50337473f2f9d
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-23 12:11:01 +0530

    Update 1.Lowest Common Ancestor of a Binary Search Tree.java

--- 10.Binary Search Trees/1.Lowest Common Ancestor of a Binary Search Tree.java
+++ 10.Binary Search Trees/1.Lowest Common Ancestor of a Binary Search Tree.java
@@ -1,5 +1,24 @@
 //gfg
+Example 1:
 
+Input:
+              5
+            /   \
+          4      6
+         /        \
+        3          7
+                    \
+                     8
+n1 = 7, n2 = 8
+Output: 7
+Example 2:
+
+Input:
+     2
+   /   \
+  1     3
+n1 = 1, n2 = 3
+Output: 2
 
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
