1a334294f8b55b3afb73f3a309d91b20e4bd26e7
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-07-26 06:15:45 +0530

    Update 12.Stack Permutations.java

--- 7.Stacks & Queues/12.Stack Permutations.java
+++ 7.Stacks & Queues/12.Stack Permutations.java
@@ -1,63 +1,3 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-
-class IntArray
-{
-    public static int[] input(BufferedReader br, int n) throws IOException
-    {
-        String[] s = br.readLine().trim().split(" ");
-        int[] a = new int[n];
-        for(int i = 0; i < n; i++)
-            a[i] = Integer.parseInt(s[i]);
-        
-        return a;
-    }
-    
-    public static void print(int[] a)
-    {
-        for(int e : a)
-            System.out.print(e + " ");
-        System.out.println();
-    }
-    
-    public static void print(ArrayList<Integer> a)
-    {
-        for(int e : a)
-            System.out.print(e + " ");
-        System.out.println();
-    }
-}
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t;
-        t = Integer.parseInt(br.readLine());
-        while(t-- > 0){
-            
-            int N; 
-            N = Integer.parseInt(br.readLine());
-            
-            
-            int[] A = IntArray.input(br, N);
-            
-            
-            int[] B = IntArray.input(br, N);
-            
-            Solution obj = new Solution();
-            int res = obj.isStackPermutation(N, A, B);
-            
-            System.out.println(res);
-            
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public static int isStackPermutation(int n, int[] ip, int[] op) {
         //Step 1 ek Stack bano
@@ -75,11 +15,7 @@ class Solution {
                 j++;
             }
         }
-
         // If the stack is empty, then the output array is a valid stack permutation of the input array
         return stack.isEmpty() ? 1 : 0;
-        
-        
     }
 }
-            
