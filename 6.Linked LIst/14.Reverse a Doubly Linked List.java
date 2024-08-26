7d0b400fb695ec4e22b46900253516cf844cec52
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-08-26 15:45:34 +0530

    Update 14.Reverse a Doubly Linked List.java

--- 6.Linked LIst/14.Reverse a Doubly Linked List.java
+++ 6.Linked LIst/14.Reverse a Doubly Linked List.java
@@ -1,55 +1,106 @@
+class Solution {
+    public DLLNode reverseDLL(DLLNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        DLLNode prev = null;
+        DLLNode temp = head;
+        while(temp != null){
+            prev = temp.prev;
+            temp.prev = temp.next;
+            temp.next = prev;
+            temp = temp.prev;
+        }
+        return prev.prev;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //gfgf
 //more better solution downside of this
 //nimisha rawat
-public static Node reverseDLL(Node  head)
-{
+// public static Node reverseDLL(Node  head)
+// {
   
-    // If the list is empty, return null
-    if (head == null) {
-        return null;
-    }
+//     // If the list is empty, return null
+//     if (head == null) {
+//         return null;
+//     }
 
-    Node current = head;  // Start with the head of the list
-    Node temp = null;     // Temporary pointer for swapping
+//     Node current = head;  // Start with the head of the list
+//     Node temp = null;     // Temporary pointer for swapping
 
-    // Traverse the entire list
-    while (current != null) {
-        // Swap the next and prev pointers of the current node
-        temp = current.prev;         // Store the previous pointer
-        current.prev = current.next; // Make prev point to next
-        current.next = temp;         // Make next point to the original prev (stored in temp)
+//     // Traverse the entire list
+//     while (current != null) {
+//         // Swap the next and prev pointers of the current node
+//         temp = current.prev;         // Store the previous pointer
+//         current.prev = current.next; // Make prev point to next
+//         current.next = temp;         // Make next point to the original prev (stored in temp)
         
-        // Move to the next node in the list (which is current.prev after swapping)
-        current = current.prev;
-    }
+//         // Move to the next node in the list (which is current.prev after swapping)
+//         current = current.prev;
+//     }
 
-    // After the loop, temp will be on the old head, 
+//     // After the loop, temp will be on the old head, 
     
-    if (temp != null) {
-        //so move one step back to get the new head
-        head = temp.prev;
-    }
+//     if (temp != null) {
+//         //so move one step back to get the new head
+//         head = temp.prev;
+//     }
 
-    return head; // Return the new head of the reversed list
-}
+//     return head; // Return the new head of the reversed list
+// }
 
 
-//more better solution
-public static Node reverseDLL(Node  head)
-{
-    Node temp = head, last = null;
+// //more better solution
+// public static Node reverseDLL(Node  head)
+// {
+//     Node temp = head, last = null;
     
-    if(head == null || head.next == null) return head;
+//     if(head == null || head.next == null) return head;
     
-    while(temp != null){
-        last = temp.prev;
-        temp.prev = temp.next;
-        temp.next = last;
+//     while(temp != null){
+//         last = temp.prev;
+//         temp.prev = temp.next;
+//         temp.next = last;
         
-        temp = temp.prev;
-    }
-//     The loop ends because temp is null
-// last points to C
-// last.prev is D, which is the new head of the reversed list
-    return last.prev;
-}
+//         temp = temp.prev;
+//     }
+// //     The loop ends because temp is null
+// // last points to C
+// // last.prev is D, which is the new head of the reversed list
+//     return last.prev;
+// }
