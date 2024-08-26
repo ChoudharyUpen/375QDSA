1a8e43f19695a1ce658019006a643cb8b8ffc983
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-08-26 16:04:30 +0530

    Update 15.Delete nodes having greater value on right.java

--- 6.Linked LIst/15.Delete nodes having greater value on right.java
+++ 6.Linked LIst/15.Delete nodes having greater value on right.java
@@ -1,24 +1,43 @@
-class Solution{
-    Node compute(Node head){
-        Node newnode = new Node(0);
-        Node again = newnode;
-        Node curr = head;
-        while(curr!=null){
-            if(!checkIf(curr)){
-                again.next = curr;
-                again = again.next; 
-            }
-            curr = curr.next;
-        }
-        return newnode.next;    
+class Solution {
+    Node compute(Node head) {
+       if(head.next==null) return head;
+       
+       head.next=compute(head.next);
+       if(head.data<head.next.data) return head.next;
+       return head;
     }
-    boolean checkIf(Node cur){
-        Node prev  = cur;
-        while(cur!=null){
-            if(prev.data<cur.data){
-                return true;
-            }
-            cur = cur.next;
-        }
-        return false;
-    }}
+}
+
+
+
+
+
+
+
+
+
+
+// class Solution{
+//     Node compute(Node head){
+//         Node newnode = new Node(0);
+//         Node again = newnode;
+//         Node curr = head;
+//         while(curr!=null){
+//             if(!checkIf(curr)){
+//                 again.next = curr;
+//                 again = again.next; 
+//             }
+//             curr = curr.next;
+//         }
+//         return newnode.next;    
+//     }
+//     boolean checkIf(Node cur){
+//         Node prev  = cur;
+//         while(cur!=null){
+//             if(prev.data<cur.data){
+//                 return true;
+//             }
+//             cur = cur.next;
+//         }
+//         return false;
+//     }}
