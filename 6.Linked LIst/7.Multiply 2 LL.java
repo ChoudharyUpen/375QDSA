74db26e49bd3c0e6e31f134529b6487f85f83c92
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-08-26 15:37:16 +0530

    Update 7.Multiply 2 LL.java

--- 6.Linked LIst/7.Multiply 2 LL.java
+++ 6.Linked LIst/7.Multiply 2 LL.java
@@ -1,23 +1,31 @@
-class GfG{
-  /*You are required to complete this method */
-   public long multiplyTwoLists(Node l1,Node l2){
-        long N = 1000000007; 
-        long num1 = 0, num2 = 0; 
-  
-        while (l1 != null || l2 !=  null){ 
-              
-            if(l1 != null){ 
-                num1 = ((num1)*10)%N + l1.data; 
-                l1 = l1.next; 
-            } 
-              
-            if(l2 != null) 
-            { 
-                num2 = ((num2)*10)%N + l2.data; 
-                l2 = l2.next; 
-            } 
-              
-        } 
-        return ((num1%N)*(num2%N))%N; 
-   }
+// Use Of MOD
+// Pehle, 2 ko 10 se multiply karo: 2 * 10 = 20
+// Ab isme 1 add karo: 20 + 1 = 21
+// Phir MOD lagao: 21 % 1000000007 = 21
+// Yahaan 21 waise toh chhota number hai, lekin agar bohot bada number 
+// hota toh MOD lagane ke baad jo remainder milta, woh relatively chhota hota, 
+// jo computer easily handle kar leta.
+
+class Solution {
+    static final long MOD = 1000000007;
+
+    public long multiplyTwoLists(Node first, Node second) {
+        long num1 = 0;
+        long num2 = 0;
+
+        // Convert the first linked list to a number with modulo operation
+        while (first != null) {
+            num1 = (num1 * 10 + first.data) % MOD;
+            first = first.next;
+        }
+
+        // Convert the second linked list to a number with modulo operation
+        while (second != null) {
+            num2 = (num2 * 10 + second.data) % MOD;
+            second = second.next;
+        }
+
+        // Multiply the two numbers and take modulo to handle large results
+        return (num1 * num2) % MOD;
+    }
 }
