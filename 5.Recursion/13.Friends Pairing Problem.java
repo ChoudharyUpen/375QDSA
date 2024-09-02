9306ce66c2f15682865e0ec5cd9d82af51fcce68
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-09-03 00:37:39 +0530

    Create 13.Friends Pairing Problem.java

---
+++ 5.Recursion/13.Friends Pairing Problem.java
@@ -0,0 +1,25 @@
+//Friends Pairing Problem
+public class q3 {
+    public static int friendsPairing(int n) {
+        // base case
+        if (n == 1 || n == 2) {
+            return n;
+        }
+        // choice
+        // single
+        int fnm1 = friendsPairing(n - 1);
+
+        // pair
+        int fnm2 = friendsPairing(n - 2);
+        int pairWays = (n - 1) * fnm2;
+
+        // totalWays
+        int totalWays = fnm1 + pairWays;
+        return totalWays;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(friendsPairing(3));
+
+    }
+}
