e313f546e280f50012aa996728816270e2ae0a1c
ChoudharyUpen <141799025+ChoudharyUpen@users.noreply.github.com>
2024-10-15 09:43:20 +0530

    Update 8.Remove nth node from end of list.java

--- 6.Linked LIst/8.Remove nth node from end of list.java
+++ 6.Linked LIst/8.Remove nth node from end of list.java
@@ -1,4 +1,40 @@
-//Leetcode
+  //Leetcode
+//imporved code keeping in mind time limit /time complexity
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode res = new ListNode(0, head);
+        ListNode dummy = res;
+
+        for (int i = 0; i < n; i++) {
+            head = head.next;
+        }
+
+        while (head != null) {
+            head = head.next;
+            dummy = dummy.next;
+        }
+
+        dummy.next = dummy.next.next;
+
+        return res.next;        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//This code is giving time limit exceed error
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode tmp=head;
