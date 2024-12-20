61ed5eae371b4f497a50027273c203eb2682065a
ChoudharyUpen <chaudharyuday1515@gmail.com>
2024-12-20 07:24:26 +0530

    Update 10.Trapping Rain Water.java

--- 1.Arrays/10.Trapping Rain Water.java
+++ 1.Arrays/10.Trapping Rain Water.java
@@ -11,11 +11,13 @@ class Solution {
 
         //current element or just pichla elemnet mai sa jo bhi bada hoga wo left[i] array mai aa jayega 
         for(int i=1;i<n;i++){
+            //isma current or pichla compare karwana hai 
             left[i]=Math.max(left[i-1],height[i]);
         }
         
         right[n-1]=height[n-1]; //means the last position in the right will be same as the position of the element .
         for(int i=n-2;i>=0;i--){
+            //isma current or usa agla compare karwana hai
             right[i]=Math.max(right[i+1],height[i]);
         }
         int ans=0; //ans variable k anadar humara file answer store hoga 
