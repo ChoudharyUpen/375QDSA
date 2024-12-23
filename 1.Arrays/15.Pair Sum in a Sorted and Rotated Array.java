bdd8f8afdd83a0f8d5c1cb33b63d4fa6996a400c
ChoudharyUpen <chaudharyuday1515@gmail.com>
2024-12-23 15:45:17 +0530

    Create 15.Pair Sum in a Sorted and Rotated Array.java

---
+++ 1.Arrays/15.Pair Sum in a Sorted and Rotated Array.java
@@ -0,0 +1,4 @@
+i found this pratice question on gfg https://www.geeksforgeeks.org/problems/pair-with-given-sum-in-a-sorted-array4940/1   but this is excatly not 
+same 
+
+full correct question solution is here :https://www.geeksforgeeks.org/given-a-sorted-and-rotated-array-find-if-there-is-a-pair-with-a-given-sum/?ref=lbp
