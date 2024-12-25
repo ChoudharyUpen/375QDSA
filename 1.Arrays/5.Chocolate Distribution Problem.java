9a5d75b2bfac17a34f53c8b5fdc09ac4ec041757
ChoudharyUpen <chaudharyuday1515@gmail.com>
2024-12-25 07:32:29 +0530

    Update 5.Chocolate Distribution Problem.java

--- 1.Arrays/5.Chocolate Distribution Problem.java
+++ 1.Arrays/5.Chocolate Distribution Problem.java
@@ -1,3 +1,25 @@
+//this solution is better and easy then solution given below it this solution does not use sliding window approach 
+class Solution {
+    public int findMinDiff(ArrayList<Integer> arr, int m) {
+        int n = arr.size();  // Use size() instead of length
+        Collections.sort(arr);  // Correct the variable name to 'arr'
+        int ans = Integer.MAX_VALUE;  // Use int since we're working with differences
+        
+        for (int i = 0; i < n - m + 1; i++) {
+            int j = i + m - 1;  
+            
+            if (arr.get(j) - arr.get(i) < ans)
+                ans = arr.get(j) - arr.get(i);
+        }
+        
+        return ans;
+    }
+}
+
+
+
+
+
 //sliding window is being used in this question 
 class Solution {
     public int findMinDiff(ArrayList<Integer> arr, int m) {
