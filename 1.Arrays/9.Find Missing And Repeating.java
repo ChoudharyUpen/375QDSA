d9618ad3087f6378dd60b5723bed9d6db94af7e5
ChoudharyUpen <chaudharyuday1515@gmail.com>
2024-12-26 08:15:47 +0530

    Update 9.Find Missing And Repeating.java

--- 1.Arrays/9.Find Missing And Repeating.java
+++ 1.Arrays/9.Find Missing And Repeating.java
@@ -1,3 +1,31 @@
+//newly updated code with differnt return type
+class Solution {
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            int abs=Math.abs(arr[i]);
+            if(arr[abs-1]>0){
+                arr[abs-1]=-arr[abs-1];
+            }
+            else{
+                ans.add(abs);
+            }
+        }
+           for(int i=0;i<arr.length;i++){
+            if(arr[i]>0){
+                ans.add(i+1);
+            }
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
+
 //is code ka kaam hai array me duplicate element aur missing number find karna
 //this is a good question practice it 
 //i have undertanded this with the help of chatgpt on 7-12-2024 now revise it so much that you dont have to learn this again from chatgpt
