9bac02dd08537e78f215026eb250b9a15005b070
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-01 20:17:13 +0530

    Create 24.Longest subarray with sum divisible by K.java

---
+++ 1.Arrays/24.Longest subarray with sum divisible by K.java
@@ -0,0 +1,19 @@
+class Solution {
+    int longSubarrWthSumDivByK(int[] a, int k) {
+        int sum = 0, maxLen = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < a.length; i++) {
+            sum+=a[i];
+            int rem = sum%k;
+            if(rem == 0) maxLen = Math.max(maxLen, i+1);
+            if(rem < 0) rem = rem + k;
+            if(map.containsKey(rem)){ 
+                maxLen = Math.max(i-map.get(rem), maxLen);    
+            }
+            else{
+                 map.put(rem, i);
+                }
+        }
+        return maxLen;
+    }
+}
