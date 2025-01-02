09aa95127d14a1d189699ed43d253e919ba12062
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-02 07:44:04 +0530

    Update 7. Next Permutation.java

--- 1.Arrays/7. Next Permutation.java
+++ 1.Arrays/7. Next Permutation.java
@@ -1,3 +1,43 @@
+//this is more better code than code downside 
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i=nums.length-2;
+        while(i>=0 && nums[i]>=nums[i+1]){
+            i--;
+        }
+        //i agar agla element sa chota ho gya toh huma wo element mil gya jisko swap karna hai 
+        if(i>=0){
+            int j=nums.length-1;
+            while(j>=0 && nums[j]<=nums[i])j--;
+            swap(nums,i,j);
+        }
+        //ab baki ka array ko reverse kar k sab sa chota bana hai 
+        reverse(nums,i+1);
+    }
+    //ab huma i sa bada elemnt mil gya ab dono ko swap kar do 
+    public void swap(int []nums,int i,int j){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public void reverse(int[]nums , int start){
+        int end=nums.length-1;
+        while(start<end){
+            swap(nums,start,end);
+            start++;
+            end--;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 class Solution {
     public void nextPermutation(int[] nums) {
         int n=nums.length;
