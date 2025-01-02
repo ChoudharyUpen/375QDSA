7f92c63b4e5c77e70f5278c11e01f5d23d7d1f2e
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-02 10:32:32 +0530

    Update 24.Longest subarray with sum divisible by K.java

--- 1.Arrays/24.Longest subarray with sum divisible by K.java
+++ 1.Arrays/24.Longest subarray with sum divisible by K.java
@@ -8,6 +8,7 @@ class Solution {
             if(rem == 0) maxLen = Math.max(maxLen, i+1);
             if(rem < 0) rem = rem + k;
             if(map.containsKey(rem)){ 
+                //subarray ko check kar rha hai present - phela jo element aya tha usko minus kar ka 
                 maxLen = Math.max(i-map.get(rem), maxLen);    
             }
             else{
