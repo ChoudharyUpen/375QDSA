849af164b99d1d9b18912742dade0afbfabefee2
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-08 22:38:04 +0530

    Update 13.Rearrange a Linked List in Zig-Zag fashion.java

--- 6.Linked LIst/13.Rearrange a Linked List in Zig-Zag fashion.java
+++ 6.Linked LIst/13.Rearrange a Linked List in Zig-Zag fashion.java
@@ -1,23 +1,22 @@
-class Solution{
-	public static Node zigZag(Node head){
-        Node temp = head;
+class Solution {
+    Node zigZag(Node head) {
+        Node temp = head; // for traversing
         int flag = 0;
-        while(temp.next!=null){
-            if(flag == 0){
-                if(temp.data>temp.next.data){
+        while (temp != null && temp.next != null) { // Ensure temp.next is not null
+            if (flag == 0) {
+                if (temp.data > temp.next.data) {
                     int val = temp.data;
                     temp.data = temp.next.data;
                     temp.next.data = val;
                 }
                 flag = 1;
-            }
-            else{
-                flag = 0;
-                if(temp.data<temp.next.data){
+            } else {
+                if (temp.data < temp.next.data) {
                     int val = temp.data;
                     temp.data = temp.next.data;
                     temp.next.data = val;
                 }
+                flag = 0;
             }
             temp = temp.next;
         }
