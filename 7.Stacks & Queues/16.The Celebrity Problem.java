a692aaed5e14a0f67be3f67ba90deaf6a606e3c7
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-12 21:16:42 +0530

    Update 16.The Celebrity Problem.java

--- 7.Stacks & Queues/16.The Celebrity Problem.java
+++ 7.Stacks & Queues/16.The Celebrity Problem.java
@@ -9,8 +9,8 @@ class Solution{
         //Step 2 Verify the Potential Celebrity
         for (int i = 0; i < n; i++) {
             //i != candidate  kud ka saath compare nahi kar rahe
-            //check if the candiate know person i
-            //check if the person i doesnot know the candiate
+            //check if the candiate know person i ---  candiate kisi ko janata hai 
+            //check if the person i doesnot know the candiate ---- ya koi candiate ko nahi janta 
             if (i != candidate && (M[candidate][i] == 1 || M[i][candidate] == 0))
                 return -1; 
         }
