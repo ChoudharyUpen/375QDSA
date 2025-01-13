af7ada1c2f6c0681fc5235f7baaa019b141be47f
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-13 07:46:53 +0530

    Update 11.Find if an expression has duplicate parenthesis or not.java

--- 7.Stacks & Queues/11.Find if an expression has duplicate parenthesis or not.java
+++ 7.Stacks & Queues/11.Find if an expression has duplicate parenthesis or not.java
@@ -1,3 +1,36 @@
+//i think below code is more easy than this code 
+class Solution {
+    public static int checkRedundancy(String s) {
+        // code here
+        Stack<Character> stack = new Stack<>();
+        int n = s.length();
+        for(int i = 0; i < n; i++){
+            char ch = s.charAt(i);
+            if(ch == ')'){
+                int flag  = 0;
+
+                    while(stack.peek() != '('){
+                        if(stack.peek() == '+' || stack.peek() == '*' || stack.peek() == '*' || stack.peek() == '-' || stack.peek() == '/') {
+                            flag = 1;
+                        }
+                        stack.pop();
+                    }
+                    stack.pop();
+
+                   //flag 0 or 1 is like elementinside 0 or 1
+                    if(flag == 0){
+                        return 1;
+                    }
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        return 0;
+    }
+}
+
+
 public class GFG {
     static boolean findDuplicateparenthesis(String s) {
         Stack<Character> Stack = new Stack<>();
