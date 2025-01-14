7981e65bd91c173616063ef7188325585a6db113
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-14 10:05:30 +0530

    Update 2.Evaluate Postfix Expression.java

--- 7.Stacks & Queues/2.Evaluate Postfix Expression.java
+++ 7.Stacks & Queues/2.Evaluate Postfix Expression.java
@@ -2,10 +2,11 @@ class Solution{
     public static int evaluatePostFix(String S){
         Stack<Integer> stack = new Stack<>();
         for (int i = 0; i < S.length(); i++) {
+           // 2 he option ho sakhta hai ya toh wo number ho ya wo digit ho 
             char c = S.charAt(i);
-          //check karenge kya vo digit hai agar hai tau stack mai push kar do
+         
             if (Character.isDigit(c)) {
-                stack.push(c - '0'); // Convert char to integer
+                stack.push(c - '0'); //Ye line digit ko integer mein convert karke stack mein daal rahi hai.
             } else {
               //agar digit nahi hai tau last 2 value of stack nikal k un pai operation perform kar do
                 int operand2 = stack.pop();
@@ -24,6 +25,5 @@ class Solution{
                         stack.push(operand1 / operand2);
                         break;
                 }}}
-        //Final answer ko return kar do sare operation ho jane k baad
         return stack.pop();
     }}
