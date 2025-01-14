8c9d76719f8742150751319f4d3bad1daeeb7ed5
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-14 07:15:10 +0530

    Update 21.Circular Tour.java

--- 7.Stacks & Queues/21.Circular Tour.java
+++ 7.Stacks & Queues/21.Circular Tour.java
@@ -1,14 +1,7 @@
-//very easy i find it gfg question
-class Solution
-{
-    //Function to find starting point where the truck can start to get through
-    //the complete circle without exhausting its petrol in between.
-    int tour(int petrol[], int distance[])
-    {
-	   int n = petrol.length;
-        
-        // Initialize variables
-      //STEP 1 TAKING 3 POINTERS
+class Solution{
+    int tour(int petrol[], int distance[]){
+	int n = petrol.length;
+	    
         int start = 0;
         int deficit = 0;
         int surplus = 0;
@@ -21,11 +14,6 @@ class Solution
                 surplus = 0;
             }
         }
-
-        // Check if the total surplus and deficit makes it possible to complete the circle
-      //IF ADDING BOTH -VE AND +VE NUMBER RESULT IS +VE THEN IT IS GOOD TO START 
-      //FROM THAT PETROL PUMP OTHERWISE NOT
-        return (surplus + deficit >= 0) ? start : -1;
-	   
+        return (surplus + deficit >= 0) ? start : -1;	   
     }
 }
