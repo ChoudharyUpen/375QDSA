177a01d7c0f659191352d41d0c791e8c3b99ba34
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-19 16:24:07 +0530

    Update 8. M-Coloring Problem.java

--- 5.Backtracking/8. M-Coloring Problem.java
+++ 5.Backtracking/8. M-Coloring Problem.java
@@ -1,3 +1,47 @@
+class solve {
+    public boolean isSafe(int c, int ind, boolean graph[][], int color[]) {
+        for (int i = 0; i < graph[ind].length; i++) {
+            if (graph[ind][i]) {
+                if (color[i] == c) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean f(int ind, int color[], boolean graph[][], int m, int n) {
+        if (ind == n) return true;
+
+        for (int j = 1; j <= m; j++) {
+            if (isSafe(j, ind, graph, color) && color[ind] == 0) {
+                color[ind] = j;
+                if (f(ind + 1, color, graph, m, n) == true) return true;
+                color[ind] = 0;
+            }
+        }
+        return false;
+    }
+
+    public boolean graphColoring(boolean graph[][], int m, int n) {
+        int color[] = new int[n];
+        return f(0, color, graph, m, n);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //gfg
 //gfg solutions
 class solve {
