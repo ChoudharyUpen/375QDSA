94bdd3f159bc4bb6b5d60989b55c40a01e8909b6
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-19 16:21:55 +0530

    Update 5.N-Queen Problem.java

--- 5.Backtracking/5.N-Queen Problem.java
+++ 5.Backtracking/5.N-Queen Problem.java
@@ -1,3 +1,51 @@
+class Solution {
+    static ArrayList<ArrayList<Integer>> res;
+    static int[] row = new int[10];
+    static int k = 0;
+
+    static boolean place(int r, int c) {
+        for (int prev = 0; prev < c; prev++) {
+            if (row[prev] == r || Math.abs(row[prev] - r) == Math.abs(prev - c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void bt(int c, int n) {
+        if (n == 2 || n == 3) return;
+
+        if (c == n) {
+            ArrayList<Integer> v = new ArrayList<Integer>();
+            for (int i = 0; i < n; i++) {
+                v.add(row[i] + 1);
+            }
+            res.add(v);
+            k++;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (place(i, c)) {
+                row[c] = i;
+                bt(c + 1, n);
+            }
+        }
+    }
+
+    static ArrayList<ArrayList<Integer>> nQueen(int n) {
+        res = new ArrayList<ArrayList<Integer>>();
+        bt(0, n);
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
 //gfg solutions
 class Solution{
         static ArrayList<ArrayList<Integer>> res; // Ye res list hai jismein saare solutions store honge
