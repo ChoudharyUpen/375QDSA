12ee43b609aef641dd961b40a49a32af6c52e8ac
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-19 16:20:30 +0530

    Update 2.Combination Sum.java

--- 5.Backtracking/2.Combination Sum.java
+++ 5.Backtracking/2.Combination Sum.java
@@ -1,3 +1,50 @@
+class Solution {
+    static void combination(ArrayList<Integer> nums, int start, int sum, ArrayList<Integer> sub, ArrayList<ArrayList<Integer>> res) {
+        if (sum == 0) {
+            res.add(new ArrayList<>(sub));
+            return;
+        }
+        if (sum < 0) return;
+        for (int i = start; i < nums.size(); i++) {
+            int val = nums.get(i);
+            if (i > start && val == nums.get(i - 1)) continue;
+            if (val > sum) break;
+            if (val <= sum) {
+                sub.add(val);
+                combination(nums, i, sum - val, sub, res);
+                sub.remove(sub.size() - 1);
+            }
+        }
+    }
+    
+    static ArrayList<ArrayList<Integer>> combinationSum(ArrayList<Integer> A, int B) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        Collections.sort(A);
+        combination(A, 0, B, new ArrayList<>(), res);
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //gfg
 //gfg solutions
 
