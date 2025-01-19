f7ecf51f17b4493d61ecf69d5e83870f4ef70e4d
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-19 16:22:32 +0530

    Update 6.Solve the Sudoku.java

--- 5.Backtracking/6.Solve the Sudoku.java
+++ 5.Backtracking/6.Solve the Sudoku.java
@@ -1,3 +1,58 @@
+class Solution {
+    static boolean SolveSudoku(int grid[][]) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (grid[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(grid, row, col, num)) {
+                            grid[row][col] = num;
+                            if (SolveSudoku(grid))
+                                return true;
+                            else {
+                                grid[row][col] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static boolean isValid(int[][] grid, int row, int col, int num) {
+        for (int x = 0; x < 9; x++)
+            if (grid[row][x] == num)
+                return false;
+        for (int x = 0; x < 9; x++)
+            if (grid[x][col] == num)
+                return false;
+        int startRow = row - row % 3;
+        int startCol = col - col % 3;
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                if (grid[i + startRow][j + startCol] == num)
+                    return false;
+        return true;
+    }
+
+    static void printGrid(int grid[][]) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(grid[i][j] + " ");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 //gfg
 //gfg solutions
 
