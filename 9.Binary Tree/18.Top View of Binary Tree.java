b8247b0f5c00d5f62e160aa3f2e619d618f66558
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-25 15:39:11 +0530

    Update 18.Top View of Binary Tree.java

--- 9.Binary Tree/18.Top View of Binary Tree.java
+++ 9.Binary Tree/18.Top View of Binary Tree.java
@@ -1,4 +1,54 @@
 class Solution {
+    static ArrayList<Integer> topView(Node root) {
+        Queue<Pair> q = new ArrayDeque<>();
+        Map<Integer, Integer> map = new TreeMap<>();
+        
+        q.add(new Pair(0, root));
+        while (!q.isEmpty()) {
+            Pair cur = q.poll();
+            if (!map.containsKey(cur.hd)) {
+                map.put(cur.hd, cur.node.data);
+            }
+            if (cur.node.left != null) {
+                q.add(new Pair(cur.hd - 1, cur.node.left));
+            }
+            if (cur.node.right != null) {
+                q.add(new Pair(cur.hd + 1, cur.node.right));
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            ans.add(entry.getValue());
+        }
+        return ans;
+    }
+
+    static class Pair {
+        int hd;
+        Node node;
+        
+        public Pair(int hd, Node node) {
+            this.node = node;
+            this.hd = hd;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
    //we will not use noraml traveral in it but level order traversal in it that why we will use queue
     static ArrayList<Integer> topView(Node root) {
         Queue<Pair> q=new ArrayDeque<>();
