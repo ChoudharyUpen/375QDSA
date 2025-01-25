3c6e25038a1fd1ff6f70e60605e099e1231c9f54
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-25 15:34:49 +0530

    Update 3. Subtree of Another Tree.java

--- 9.Binary Tree/3. Subtree of Another Tree.java
+++ 9.Binary Tree/3. Subtree of Another Tree.java
@@ -1,4 +1,59 @@
 class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null) {
+            return false;
+        }
+        if(helper(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    public boolean helper(TreeNode root, TreeNode subRoot) {
+        if(root == null && subRoot == null) {
+            return true;
+        }
+        if(root == null || subRoot == null) {
+            return false;
+        } 
+        if(root.val != subRoot.val) {
+            return false;
+        }
+        return helper(root.left, subRoot.left) && helper(root.right, subRoot.right);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
     // Hum is sawal ko recursively solve karenge
     //Case 1: Null aur !Null 
     //Case 2: Null aur Null
