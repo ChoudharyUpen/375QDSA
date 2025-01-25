5a999f1309be03d9eda4bfa4c9c0327c501f02fd
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-25 15:37:48 +0530

    Update 15.Construct Binary Tree from Preorder and Inorder Traversal.java

--- 9.Binary Tree/15.Construct Binary Tree from Preorder and Inorder Traversal.java
+++ 9.Binary Tree/15.Construct Binary Tree from Preorder and Inorder Traversal.java
@@ -1,4 +1,42 @@
 class Solution {
+    private int p = 0; 
+    private int i = 0; 
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return build(preorder, inorder, Integer.MIN_VALUE);
+    }
+
+    private TreeNode build(int[] preorder, int[] inorder, int stop) {
+        if (p >= preorder.length) {
+            return null;
+        }
+        if (inorder[i] == stop) {
+            ++i;
+            return null;
+        }
+        TreeNode node = new TreeNode(preorder[p++]);
+        node.left = build(preorder, inorder, node.val);
+        node.right = build(preorder, inorder, stop);
+        return node;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
     private int p = 0; // preorder index
     private int i = 0; // inorder index
 
