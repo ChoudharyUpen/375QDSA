b3a99df586d34b0e2776edc0d29aa36eb44f7e90
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-29 16:20:29 +0530

    Update 1.Maximum depth of Binary tree.java

--- 9.Binary Tree/1.Maximum depth of Binary tree.java
+++ 9.Binary Tree/1.Maximum depth of Binary tree.java
@@ -1,10 +1,10 @@
 //Learned From Algorithm made easy youtbe chan
 
-
 class Solution {
-    public int maxDepth(TreeNode root){
-        if(root ==null) return 0;
-        int l=maxDepth(root.left);
-        int r=maxDepth(root.right);
-        return 1+Math.max(l,r);
-        }}}
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+        return 1 + Math.max(l, r);
+    }
+}
