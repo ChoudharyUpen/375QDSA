88c641a4a1bf815cd171a10118b8a97fe23185ff
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-31 10:43:28 +0530

    Update 6.Array is sorted or not .java

--- 5.Recursion/6.Array is sorted or not .java
+++ 5.Recursion/6.Array is sorted or not .java
@@ -1,3 +1,27 @@
+public class q6 {
+    public static boolean isSorted(int arr[], int i) {
+        if (i == arr.length - 1) {
+            return true;
+        }
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+        return isSorted(arr, i + 1);
+    }
+    public static void main(String Args[]) {
+        int arr[] = { 1, 2, 3, 4 };
+        System.out.println(isSorted(arr, 0));
+    }
+}
+
+
+
+
+
+
+
+
+
 //Check Array is Sorted Or Not
 public class q6 {
     public static boolean isSorted(int arr[], int i) {
