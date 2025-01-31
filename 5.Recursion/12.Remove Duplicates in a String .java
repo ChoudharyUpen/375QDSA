157b6c91834be49ac42b0d68e9cefc101097f773
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-01-31 10:06:57 +0530

    Update 12.Remove Duplicates in a String .java

--- 5.Recursion/12.Remove Duplicates in a String .java
+++ 5.Recursion/12.Remove Duplicates in a String .java
@@ -1,3 +1,44 @@
+public class Q2 {
+    public static void removveDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (map[currChar - 'a'] == true) {
+            removveDuplicates(str, idx + 1, newStr, map);
+        } else {
+            map[currChar - 'a'] = true;
+            removveDuplicates(str, idx+1, newStr.append(currChar), map);
+        }
+    }
+    public static void main(String[] args) {
+        String str="appnnacollege";
+        removveDuplicates(str, 0, new StringBuilder(" "), new boolean[26]);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Remove Duplicates in a String   
 public class Q2 {
     public static void removveDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
