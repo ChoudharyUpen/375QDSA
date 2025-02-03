85dabba8eed3135a9384916879f653dc6def236a
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-02-03 21:26:16 +0530

    Update 28.Duplicate Subtree.java

--- 9.Binary Tree/28.Duplicate Subtree.java
+++ 9.Binary Tree/28.Duplicate Subtree.java
@@ -1,5 +1,51 @@
 class Solution{
 
+    public List<Node> printAllDups(Node root) {
+       HashMap<String, Integer> s = new HashMap<>();
+       List<Node> l = new ArrayList<>();
+       helper(root, l, s);
+       Collections.sort(l, (a, b) -> a.data - b.data);
+       return l;
+    }
+
+    public String helper(Node root, List<Node> l, HashMap<String, Integer> s) {
+        if(root == null) return "";
+        
+        String left = helper(root.left, l, s);
+        String right = helper(root.right, l, s);
+        
+        String curr = left + "-" + root.data + "-" + right;
+        if(s.getOrDefault(curr, 0) == 1) {
+            l.add(root);
+        }
+        s.put(curr, s.getOrDefault(curr, 0) + 1);
+        return curr;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution{
+
     // Method to print all duplicate subtrees
     public List<Node> printAllDups(Node root)
     {
