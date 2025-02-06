29f6dbe0bff5a3859551f675417d89f8156820ce
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-02-06 09:22:24 +0530

    Update 3.Top K Frequent Elements.java

--- 11.Heap & Hashing/3.Top K Frequent Elements.java
+++ 11.Heap & Hashing/3.Top K Frequent Elements.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer>[] bucket = new List[nums.length + 1];
         HashMap<Integer, Integer> hm = new HashMap<>();
         for (int num : nums) {
             hm.put(num, hm.getOrDefault(num, 0) + 1);
         }
+         List<Integer>[] bucket = new List[nums.length + 1];
         for (int key : hm.keySet()) {
             int freq = hm.get(key);
             if (bucket[freq] == null) {
