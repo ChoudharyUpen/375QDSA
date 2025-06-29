63d24b868dcbe0df14e8da6ba74eae1a524b9c21
ChoudharyUpen <chaudharyuday1515@gmail.com>
2025-06-29 14:12:26 +0530

    Update 1.Reverse a Linked List.java

--- 6.Linked LIst/1.Reverse a Linked List.java
+++ 6.Linked LIst/1.Reverse a Linked List.java
@@ -1,3 +1,24 @@
+//this solution is better than the solution below it 
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        while(head!=null){
+            ListNode temp =head.next;
+            head.next=prev;
+            prev=head;
+            head=temp;
+        }
+        return prev;
+    }
+}
+
+
+
+
+
+
+
+
 Reverse a Linked List ------------
 (we use three pointer in this we can also this of a boat example in 
 which only middle one is doing something and rest all are moving)
